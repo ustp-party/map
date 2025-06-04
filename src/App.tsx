40af4fabd4fa5569d43a777b9946d79e8e351499
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-06-04 07:18:03 +0000

    setting up the desktop sidebar (to implement) to only appear when viewport width is > 600

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
 import "./App.css";
 import Map from "./components/Map";
 import Searchbar from "./components/searchbar/Searchbar";
+import DesktopSidebar from "./components/sidebar/DesktopSidebar";
+import { useState, useEffect } from "react";
+
+// Custom hook for viewport width
+function useViewportWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return width;
+}
 
 function App() {
+  const vpWidth = useViewportWidth();
+  const [detailsOpen, setDetailsOpen] = useState(false);
+
+  const sidebarClass = [
+    "sidebar-container",
+    detailsOpen && "open"
+  ]
+    .filter(Boolean)
+    .join(" ");
+
   return (
     <div className="App">
-      <div className="searchbar-container">
-        <Searchbar />
-      </div>
-      <div className="map-wrapper">
-        <Map />
+      <div className="container">
+        <div className="searchbar-container">
+          <Searchbar />
+        </div>
+
+        {vpWidth > 600 && (
+          <div className={sidebarClass}>
+            <DesktopSidebar />
+          </div>
+        )}
+
+        <div className="map-wrapper">
+          <Map />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/sidebar/DesktopSidebar.tsx
@@ -0,0 +1,9 @@
+import style from './DesktopSidebar.module.css';
+
+export default function Sidebar() {
+  return (
+    <>
+    {/* Here */}
+    </>
+  );
+}
