0d1b675bea40374312259f09f6565deac3b26404
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-06-04 01:27:42 +0000

    Refactor Vite configuration for improved readability and consistency, added directory aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-  watch: {
-   usePolling: true,
+    watch: {
+      usePolling: true,
+    },
+    host: true, // Here
+    strictPort: true,
+    port: 8080,
   },
-  host: true, // Here
-  strictPort: true,
-  port: 8080, 
-}
-})
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": "/src/components",
+      "@utils": "/src/utils",
+      "@assets": "/src/assets",
+    },
+  },
+});
