0a9c28667460d4762b141c58d771668ecaa455cc
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-06-04 04:51:54 +0000

    refactor: simplify boolean operation

--- src/components/material/SearchIcon.tsx
+++ src/components/material/SearchIcon.tsx
@@ -5,20 +5,9 @@ import searchLight from "@assets/material/search-24-light.svg";
 // TODO: Find a way to make this component more dynamic
 // less boilerplate hardcoded icon importing
 export default function SearchIcon() {
-  if (prefersDarkMode()) {
-    return (
-      <img
-        src={searchDark}
-        alt="Search"
-        className="material-icons"
-        style={{ width: "24px", height: "24px" }}
-      />
-    );
-  }
-
   return (
     <img
-      src={searchLight}
+      src={prefersDarkMode() ? searchDark : searchLight}
       alt="Search"
       className="material-icons"
       style={{ width: "24px", height: "24px" }}
