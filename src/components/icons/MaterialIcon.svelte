b280b209a65a407fb2a87a9417b035eb13419cae
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-06-13 21:57:49 +0800

    fix: update icon path to include base URL

--- src/components/icons/MaterialIcon.svelte
+++ src/components/icons/MaterialIcon.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
   import { prefersDarkMode } from "$lib/utils/theme";
   import { onMount } from "svelte";
+  import { base } from "$app/paths";
 
   let props = $props<{
     name: string;
   }>();
 
-  const ICONS_DIR = "/material-icons";
+  const ICONS_DIR = "material-icons";
 
   let icon = $state<string>("");
 
   onMount(() => {
+    console.log(base);
     icon = prefersDarkMode()
-      ? `${ICONS_DIR}/${props.name}-24-dark.svg`
-      : `${ICONS_DIR}/${props.name}-24-light.svg`;
+      ? `${base}/${ICONS_DIR}/${props.name}-24-dark.svg`
+      : `${base}/${ICONS_DIR}/${props.name}-24-light.svg`;
   });
 </script>
 
