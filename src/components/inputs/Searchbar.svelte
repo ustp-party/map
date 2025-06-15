ef693931e3f47a810c4e55b413f095010774c6aa
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-06-15 21:44:06 +0800

    feat: enhance Searchbar component with clear input functionality and add close icon

--- src/components/inputs/Searchbar.svelte
+++ src/components/inputs/Searchbar.svelte
@@ -1,13 +1,37 @@
 <script lang="ts">
-  import SearchIcon from "$components/icons/SearchIcon.svelte";
+  import SvgIcon from "$components/icons/SVGIcon.svelte";
+  import closeSVG from "$assets/material-icons/close.svg?raw";
+  import searchSVG from "$assets/material-icons/search-24.svg?raw";
   import { getSearchbarInputState } from "$lib/stores/SearchInputState.svelte";
+  import { fade } from "svelte/transition";
 
   const searchbarInputState = getSearchbarInputState();
 </script>
 
-<div class="searchbar">
-  <input type="text" placeholder="Search" bind:value={searchbarInputState.value} />
-  <SearchIcon />
+<div class="searchbar" transition:fade={{ duration: 200 }}>
+  <input
+    type="text"
+    placeholder="Search"
+    bind:value={searchbarInputState.value}
+  />
+  <button aria-label="Search">
+    <SvgIcon size={24} alt="Search">
+      {@html searchSVG}
+    </SvgIcon>
+  </button>
+  {#if searchbarInputState.value.length > 0}
+    <div transition:fade={{ duration: 200 }}>
+      <button
+        class="clear-input"
+        aria-label="Clear search input"
+        onclick={() => searchbarInputState.update("")}
+      >
+        <SvgIcon size={24} alt="Clear search input">
+          {@html closeSVG}
+        </SvgIcon>
+      </button>
+    </div>
+  {/if}
 </div>
 
 <style lang="scss">
@@ -38,6 +62,22 @@
       &::placeholder {
         color: var(--text);
       }
+    }
+  }
+
+  button {
+    all: unset;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    color: var(--icon-subtle);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+      color: var(--icon-color);
+      background-color: transparent;
     }
   }
   @media (prefers-color-scheme: dark) {
