5dfd87b99fa18c2a820256932b28378aa74bd45a
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-06-28 08:42:16 +0800

    fix: enhance footer with contact options and additional links for user support

--- src/components/sidebars/SidebarContent.svelte
+++ src/components/sidebars/SidebarContent.svelte
@@ -29,14 +29,27 @@
     <h4>Results ({searchState.results.length})</h4>
 
     <ResultCards features={searchState.results} />
-    <p class="support-message">
-      Not what you were looking for?<br />Contact us by posting an
-      <a href="https://github.com/ustp-party/map/issues/new/choose">issue</a>
-      or by sending an email to
-      <a href="mailto:chrisandrei.irag@1.ustp.edu.ph"
-        >chrisandrei.irag@1.ustp.edu.ph</a
-      >
-    </p>
+    <footer class="footer">
+      <div>
+        Not what you were looking for or the data is wrong?<br />Contact us by
+        posting an
+        <a href="https://github.com/ustp-party/map/issues/new/choose">issue</a>
+        or by sending an email to
+        <a href="mailto:chrisandrei.irag@1.ustp.edu.ph"
+          >chrisandrei.irag@1.ustp.edu.ph</a
+        >
+      </div>
+      <div class="other-links">
+        <a
+          href="https://github.com/ustp-party/map/blob/master/.github/CONTRIBUTING.md"
+          >Contribute</a
+        >
+        <a
+          href="https://www.ustp.edu.ph/cdeo/ustp-cagayan-de-oro-360-virtual-tour/"
+          >Official 360 Virtual Tour</a
+        >
+      </div>
+    </footer>
   </div>
 {:else}
   <div class="introduction">
@@ -109,10 +122,40 @@
     }
   }
 
-  .support-message {
+  .footer {
     padding: 10px;
     text-align: center;
     color: var(--text-secondary);
-    font-size: 0.875rem;
+    font-size: small;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+
+    .other-links {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      gap: 8px;
+
+      a {
+        font-weight: 500;
+        color: var(--text-secondary);
+        text-decoration: none;
+
+        &::before {
+          content: "[";
+          color: var(--text);
+        }
+        &::after {
+          content: "]";
+          color: var(--text);
+        }
+
+        &:hover {
+          text-decoration: underline;
+          color: var(--text);
+        }
+      }
+    }
   }
 </style>
